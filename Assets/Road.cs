9bd754666ee1a9eb891c6c62b88ad4533990e110
Julian Hernandez <gmfk07@gmail.com>
2020-11-11 14:52:02 -0500

    Road Example

---
+++ Assets/Road.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Road : MonoBehaviour
+{
+    public float roadLength;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.localScale = new Vector3(1, 1, roadLength);
+    }
+}
---
+++ Assets/RoadSpawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadSpawner : MonoBehaviour
+{
+    public GameObject RoadObject;
+    public int cycles = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 1; i < cycles; i++)
+        {
+            GameObject newObject = Instantiate(RoadObject, new Vector3(i * 2, 0, 0), Quaternion.identity);
+            Road newRoad = newObject.GetComponent<Road>();
+            newRoad.roadLength = i;
+        }
+    }
+}
